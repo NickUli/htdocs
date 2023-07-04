afd31200c1ba9865a811b5ca15fe6ace356680a7
NickUli <nickuli@vivaldi.net>
2023-07-04 22:10:26 +0300

    adding main container in login.php

--- web/pages/login.php
+++ web/pages/login.php
@@ -30,5 +30,24 @@
         </div>
     </div>
 </nav>
+<div class="container">
+    <h2 class="mt-4">Sign in</h2>
+    <form class="mt-4">
+        <div class="form-group">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 </body>
 </html>
